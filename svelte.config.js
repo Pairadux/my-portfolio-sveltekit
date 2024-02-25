18e8d28195efde37ec174e53ce46015849d78984
AustinCGause <austincgause@gmail.com>
2024-02-24 22:11:29 -0500

    re-added CNAME

--- svelte.config.js
+++ svelte.config.js
@@ -10,9 +10,9 @@ const config = {
 		adapter: adapter({
 			fallback: '404.html',
 		}),
-    paths: {
-      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-    }
+    // paths: {
+    //   base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+    // }
 	}
 };
 
