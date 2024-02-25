139c00194c253196f687f9f1ae74e1d0e23fb8aa
AustinCGause <austincgause@gmail.com>
2024-02-24 20:37:26 -0500

    attempted to fix config issues

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 
@@ -13,7 +15,10 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
+		}),
+    paths: {
+      base: dev ? '' : process.env.BASE_PATH,
+    }
 	}
 };
 
