2370747b055c474753aad9d22586b4c4a538b64c
AustinCGause <austincgause@gmail.com>
2024-02-28 01:56:49 -0500

    added tooltips to skills

--- src/routes/components/Hero.svelte
+++ src/routes/components/Hero.svelte
@@ -25,7 +25,7 @@
         },
         {
             icon: "tailwind.svg",
-            name: "Tailwind CSS",
+            name: "TailwindCSS",
             description: "A utility-first CSS framework used to rapidly build custom user interfaces. It provides low-level utility classes to build designs directly in your markup.",
         },
         {
@@ -102,20 +102,17 @@
         <h2 class="text-center text-2xl font-semibold leading-tight tracking-tight">Notable Skills</h2>
 
         <p class="mb-4 hidden text-center font-light leading-none tracking-tight text-neutral-500 md:block">Hover for details</p>
-        <!-- <p class="mb-6 text-center font-light leading-none tracking-tight text-neutral-500 md:hidden">Tap for details</p> -->
+        <p class="mb-6 text-center font-light leading-none tracking-tight text-neutral-500 md:hidden">Tap for details</p>
 
         <!-- SKILLS -->
         <div class="grid grid-cols-5 gap-4">
             {#each skills as skill}
-                <div class="group relative h-12 w-12 md:h-14 md:w-14">
+                <div class="tooltip h-12 w-12 md:h-14 md:w-14">
                     <img
                         class="h-14 w-14"
                         src={`${assets}/images/icons/skills/${skill.icon}`}
                         alt="" />
-
-                    <!-- figure out how to get the tooltip working -->
-                    <!-- after:absolute after:top-full after:left-1/2 after:transform after:translate-x-1/2 after:border-4 after:border-solid after:border-transparent -->
-                    <!-- <span class="absolute -top-12 left-1/2 z-20 w-12 -translate-x-1/2 transform rounded-md bg-black p-1 text-center text-white">Test</span> -->
+                    <span class="tooltiptext">{skill.name}</span>
                 </div>
             {/each}
         </div>
@@ -155,3 +152,40 @@
         </svg>
     </div>
 </section>
+
+<style>
+    .tooltip {
+        position: relative;
+        display: inline-block;
+    }
+
+    .tooltip .tooltiptext {
+        visibility: hidden;
+        width: 120px;
+        bottom: 110%;
+        left: 50%;
+        margin-left: -60px;
+        background-color: black;
+        color: #fff;
+        text-align: center;
+        padding: 5px 0;
+        border-radius: 6px;
+        position: absolute;
+        z-index: 1;
+    }
+
+    .tooltip:hover .tooltiptext {
+        visibility: visible;
+    }
+
+    .tooltip .tooltiptext::after {
+        content: " ";
+        position: absolute;
+        top: 100%;
+        left: 50%;
+        margin-left: -5px;
+        border-width: 5px;
+        border-style: solid;
+        border-color: black transparent transparent transparent;
+    }
+</style>
