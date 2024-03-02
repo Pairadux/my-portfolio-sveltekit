bf18f63b07b518b75535100ef545cd3066a6bb5d
Colt <austincgause@gmail.com>
2024-03-01 20:58:14 -0500

    updated resume page to be markdown rather than an image

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,28 +1,108 @@
-<script lang="ts">
-    import { assets } from "$app/paths";
-</script>
+<!-- 
+
+Everything Newest to Oldest
+
+Summary (possibly going to omit)
+
+Professional History
+
+Education
+
+Skills
+
+Awards and Achievements
+
+Published Works
+
+-->
 
 <section class="my-8 flex flex-col items-center">
-    <!-- RESUME IMAGE -->
-    <img
-        class="md:w-3/4 lg:w-1/2"
-        src="{assets}/images/resume/AustinGauseResume.jpg"
-        alt="" />
+    <article class="container prose prose-sm prose-neutral px-4 md:prose-base lg:prose-lg xl:prose-xl 2xl:prose-2xl">
+        <!-- EDUCATION -->
+        <h2 class="text-center">Education</h2>
+        <p>
+            <span><strong>Valdosta State University, GPA 3.75</strong></span><span class="float-right"><strong>Valdosta, GA</strong></span>
+            <br />
+            <span><em>B.S. Computer Science, Math Minor</em></span><span class="float-right"><em>May 2026</em></span>
+        </p>
+        <!-- <ul> -->
+        <!--     <li>Dean's List (Fall 2022 - Fall 2023)</li> -->
+        <!-- </ul> -->
+        <p><span><strong>Bainbridge High School, GPA 3.89</strong></span><span class="float-right"><strong>Bainbridge, GA</strong></span></p>
+        <!-- <ul> -->
+        <!--     <li>Zell Miller Recipient</li> -->
+        <!--     <li>Principle's Award</li> -->
+        <!--     <li>National Honor Society Member</li> -->
+        <!--     <li>Honor & AP Course Load</li> -->
+        <!--     <li>1250 Cumulative SAT Score</li> -->
+        <!-- </ul> -->
 
-    <!-- DOWNLOAD OPTIONS -->
-    <div class="mt-8 flex">
-        <p class="text-xl font-medium">
-            <a
-                class="text-blue-500"
-                href="{assets}/images/resume/AustinGauseResume.jpg"
-                download="AustinGauseResume">JPG</a>
+        <!-- EXPERIENCE -->
+        <h2 class="text-center">Experience</h2>
+        <p>
+            <span><strong>Self-Employed</strong></span><span><strong class="float-right">Valdosta, GA</strong></span>
+            <br />
+            <span><em>Freelance Web Developer</em></span><span class="float-right"><em>Current</em></span>
         </p>
-        <p class="mx-2 text-xl font-medium">|</p>
-        <p class="text-xl font-medium">
-            <a
-                class="text-blue-500"
-                href="{assets}/images/resume/AustinGauseResume.pdf"
-                download="AustinGauseResume">PDF</a>
+        <ul>
+            <li>Utilizes various design and web development technologies and tools to create high-quality websites that fulfill client’s requests and needs.</li>
+        </ul>
+        <p>
+            <span><strong>ProcessPlan</strong></span><span><strong class="float-right">Valdosta, GA</strong></span>
+            <br />
+            <span><em>Robot Trainer</em></span><span class="float-right"><em>June 2023 - Nov 2023</em></span>
         </p>
-    </div>
+        <ul>
+            <li>Utilized proprietary software to complete designated tasks to meet client needs and expectations in a timely manner.</li>
+            <li>Interacted with superiors frequently to ensure accurate utilization of proprietary software and completion of client requests.</li>
+        </ul>
+        <p>
+            <span><strong>New Media Center</strong></span><span><strong class="float-right">Valdosta, GA</strong></span>
+            <br />
+            <span><em>Student Technician</em></span><span class="float-right"><em>Sep 2022 - Oct 2023</em></span>
+        </p>
+        <ul>
+            <li>Managed multiple Excel spreadsheets for inventory management purposes.</li>
+            <li>Collaborated with other student technicians to complete various digital media projects.</li>
+            <li>Implemented and utilized multiple systems to provide smooth equipment rental and check-out to patrons.</li>
+        </ul>
+        <p>
+            <span><strong>The Home Depot</strong></span><span><strong class="float-right">Bainbridge, GA</strong></span>
+            <br />
+            <span><em>Customer Service - Flooring</em></span><span class="float-right"><em>Oct 2021 - Aug 2022</em></span>
+        </p>
+        <ul>
+            <li>Utilized Point of Sale (POS) Systems to facilitate transactions via cash, check, and credit/debit.</li>
+            <li>Assisted in the development and execution of strategies to garner leads, promote key products, display on-sale items, and market seasonal merchandise.</li>
+            <li>Attained multiple lift-equipment licenses to better assist co-workers and provide a smoother experience for customers purchasing heavy or bulk merchandise.</li>
+        </ul>
+        <p>
+            <span><strong>Johnny Gause Flooring</strong></span><span><strong class="float-right">Bainbridge, GA</strong></span>
+            <br />
+            <span><em>LVP Installer</em></span><span class="float-right"><em>Sep 2019 - Oct 2021</em></span>
+        </p>
+        <ul></ul>
+
+        <!-- SKILLS -->
+        <h2 class="text-center">Skills</h2>
+        <p>Microsoft Office Programs (Word, PowerPoint, Excel, Outlook, Access), Adobe Programs (Photoshop, Premiere Pro, Illustrator), Web Development Technologies (HTML, CSS, JS), Computing Systems</p>
+
+        <!-- ACTIVITIES -->
+        <h2 class="text-center">Activities</h2>
+        <ul>
+            <li>Fortnite</li>
+        </ul>
+
+        <!-- CERTIFICIATIONS & ACHIEVEMENTS -->
+        <h2 class="text-center">Certifications & Achievements</h2>
+        <p>
+            <span>Python</span><span class="float-right">Codecademy 2022</span>
+            <br />
+            <span>Web Development, Python</span><span class="float-right">Educative 2023</span>
+            <br />
+            <span>MS Office</span>
+            <br />
+            <span>Dean's List</span>
+        </p>
+    </article>
 </section>
