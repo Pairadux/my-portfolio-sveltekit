ff341810e200d229e7009c62fc167a1458f734d5
Colt <austincgause@gmail.com>
2024-03-04 14:43:52 -0500

    updated resume and footer symbols

--- src/lib/footer/Footer.svelte
+++ src/lib/footer/Footer.svelte
@@ -1,30 +1,26 @@
 <script lang="ts">
-
     const date = new Date();
 
     let year = date.getFullYear();
-
 </script>
 
 <!-- FOOTER -->
-<footer class="bg-white py-6 shadow-inner flex justify-center"> 
-
+<footer class="flex justify-center bg-white py-6 shadow-inner">
     <div class="container flex justify-center">
         <!-- <ul class="flex flex-row gap-4"> -->
-            <!-- {% for link, viewbox, icon in social_media_svgs % -->
-            <!---->
-            <!-- <li class="h-12 w-12 rounded-full flex justify-center group hover:bg-neutral-950 items-center ring-2 ring-neutral-950"> -->
-            <!--     <a href="{{ link }}"> -->
-            <!--         <svg class="h-8 w-8" xmlns="http://www.w3.org/2000/svg" viewBox="{{ viewbox }}"><!-- ! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> 
-            <!--             <path class="group-hover:fill-neutral-50" d="{{ icon }}"/> -->
-            <!--         </svg> -->
-            <!--     </a> -->
-            <!-- </li> -->
-            <!---->
-            <!-- {% endfor %  -->
+        <!-- {% for link, viewbox, icon in social_media_svgs % -->
+        <!---->
+        <!-- <li class="h-12 w-12 rounded-full flex justify-center group hover:bg-neutral-950 items-center ring-2 ring-neutral-950"> -->
+        <!--     <a href="{{ link }}"> -->
+        <!--         <svg class="h-8 w-8" xmlns="http://www.w3.org/2000/svg" viewBox="{{ viewbox }}"><!-- ! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+        <!--             <path class="group-hover:fill-neutral-50" d="{{ icon }}"/> -->
+        <!--         </svg> -->
+        <!--     </a> -->
+        <!-- </li> -->
+        <!---->
+        <!-- {% endfor %  -->
         <!-- </ul> -->
 
-        <h4 class="text-black">Site made by me <svg class="inline-block" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!-- ! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM199.4 312.6c-31.2-31.2-31.2-81.9 0-113.1s81.9-31.2 113.1 0c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9c-50-50-131-50-181 0s-50 131 0 181s131 50 181 0c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0c-31.2 31.2-81.9 31.2-113.1 0z"/></svg> {year}</h4>
+        <h4 class="text-black">Site made by me &#169; {year}</h4>
     </div>
-
 </footer>
--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -17,9 +17,9 @@ Published Works
 -->
 
 <section class="my-8 flex flex-col items-center">
-    <article class="container prose prose-sm prose-neutral px-4 md:prose-base lg:prose-lg xl:prose-xl 2xl:prose-2xl">
+    <article class="container prose prose-lg prose-neutral px-4 md:prose-xl">
         <!-- EDUCATION -->
-        <h2 class="text-center">Education</h2>
+        <h3 class="text-center">Education</h3>
         <p>
             <span><strong>Valdosta State University, GPA 3.75</strong></span><span class="float-right"><strong>Valdosta, GA</strong></span>
             <br />
@@ -28,7 +28,11 @@ Published Works
         <!-- <ul> -->
         <!--     <li>Dean's List (Fall 2022 - Fall 2023)</li> -->
         <!-- </ul> -->
-        <p><span><strong>Bainbridge High School, GPA 3.89</strong></span><span class="float-right"><strong>Bainbridge, GA</strong></span></p>
+        <p>
+            <span><strong>Bainbridge High School, GPA 3.89</strong></span><span class="float-right"><strong>Bainbridge, GA</strong></span>
+            <br />
+            <span><em>High School Diploma</em></span><span class="float-right"><em>May 2022</em></span>
+        </p>
         <!-- <ul> -->
         <!--     <li>Zell Miller Recipient</li> -->
         <!--     <li>Principle's Award</li> -->
@@ -38,14 +42,15 @@ Published Works
         <!-- </ul> -->
 
         <!-- EXPERIENCE -->
-        <h2 class="text-center">Experience</h2>
+        <h3 class="text-center">Experience</h3>
         <p>
             <span><strong>Self-Employed</strong></span><span><strong class="float-right">Valdosta, GA</strong></span>
             <br />
             <span><em>Freelance Web Developer</em></span><span class="float-right"><em>Current</em></span>
         </p>
         <ul>
-            <li>Utilizes various design and web development technologies and tools to create high-quality websites that fulfill client’s requests and needs.</li>
+            <li>Design and develop high quality websites using a variety of web development tools and frameworks to meet or exceed customer requirements and expectations.</li>
+            <li>Work directly with clients to ensure that projects are in-line with their vision, and that development is meeting their expectations both in terms of time and quality.</li>
         </ul>
         <p>
             <span><strong>ProcessPlan</strong></span><span><strong class="float-right">Valdosta, GA</strong></span>
@@ -65,6 +70,7 @@ Published Works
             <li>Managed multiple Excel spreadsheets for inventory management purposes.</li>
             <li>Collaborated with other student technicians to complete various digital media projects.</li>
             <li>Implemented and utilized multiple systems to provide smooth equipment rental and check-out to patrons.</li>
+            <li>Assisted in the imaging and troubleshooting of various equipment, including cameras and laptops.</li>
         </ul>
         <p>
             <span><strong>The Home Depot</strong></span><span><strong class="float-right">Bainbridge, GA</strong></span>
@@ -84,26 +90,33 @@ Published Works
         <ul></ul>
 
         <!-- SKILLS -->
-        <h2 class="text-center">Skills</h2>
-        <p>Microsoft Office Programs (Word, PowerPoint, Excel, Outlook, Access), Adobe Programs (Photoshop, Premiere Pro, Illustrator), Web Development Technologies (HTML, CSS, JS), Computing Systems</p>
+        <h3 class="text-center">Skills</h3>
+        <p>Web Development Technologies (HTML, CSS, JS, Tailwindcss, Svelte, Bootstrap, Bulma, NodeJS), Computer Systems (Windows, Linux, MacOS), Command Line Operation, Java, Python, Rust, Adobe Products, Microsoft Office, Visual Studio Code, Neovim, JetBrains, Data Management, API Querying</p>
 
         <!-- ACTIVITIES -->
-        <h2 class="text-center">Activities</h2>
-        <ul>
-            <li>ACM (Association of Computing Machinery) Treasurer</li>
-            <li>ACM (Association of Computing Machinery) Member</li>
-        </ul>
+        <h3 class="text-center">Activities</h3>
+        <p>
+            <span>ACM (Association of Computing Machinery) - Member</span><span class="float-right">Current</span>
+            <br />
+            <span>ACM (Association of Computing Machinery) - Treasurer</span><span class="float-right">2023 - 2023&#8224;</span>
+            <br />
+            <span>Housing Move In Volunteer</span><span class="float-right">Fall 2023</span>
+        </p>
 
         <!-- CERTIFICIATIONS & ACHIEVEMENTS -->
-        <h2 class="text-center">Certifications & Achievements</h2>
+        <h3 class="text-center">Certifications & Achievements</h3>
         <p>
             <span>Python</span><span class="float-right">Codecademy 2022</span>
             <br />
             <span>Web Development, Python</span><span class="float-right">Educative 2023</span>
             <br />
-            <span>MS Office</span>
+            <span>MS Office</span><span class="float-right">Certiport 2020</span>
             <br />
-            <span>Dean's List</span>
+            <span>Dean's List</span><span class="float-right">Fall 2022 - Fall 2023</span>
         </p>
+
+        <!-- FOOTNOTE -->
+        <hr />
+        <p class="text-sm">&#8224;Had to step down from my position early due to multiple time-constraints I was experiencing at the time.</p>
     </article>
 </section>
