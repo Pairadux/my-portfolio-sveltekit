4b37b84bacf68e6830ac2ef0e1a7117b5b1666bb
AustinCGause <austincgause@gmail.com>
2024-03-03 22:52:34 -0500

    added wip dialog popup

---
+++ src/lib/popups/DevelopmentPopup.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<dialog open class="fixed top-0 left-0 h-screen w-screen z-50">
+    <div class="flex items-center gap-2 justify-center flex-col h-full w-full bg-white">
+        <h2 class="text-2xl">This site is a work in progress</h2>
+        <p class="w-1/2 text-center">This site is in an incomplete state, however, if you still wish to proceed, please click the button below</p>
+        <form method="dialog">
+            <button class="p-2 bg-black text-white rounded-lg">Proceed</button>
+        </form>
+    </div>
+</dialog>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import Header from "$lib/header/Header.svelte";
   import Footer from "$lib/footer/Footer.svelte";
+
+  import DevelopmentPopup from "$lib/popups/DevelopmentPopup.svelte";
 </script>
 
+<DevelopmentPopup />
 <div class="min-h-screen box-border">
     <Header/>
     <slot></slot>
