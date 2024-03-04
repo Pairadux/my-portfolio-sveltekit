ae7a1a14ea2d64721e73925406ac2d07f640286f
Colt <austincgause@gmail.com>
2024-03-04 14:06:01 -0500

    added an alert icon to popup

--- src/lib/popups/DevelopmentPopup.svelte
+++ src/lib/popups/DevelopmentPopup.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
+    import { assets } from "$app/paths";
 </script>
 
 <dialog open class="fixed top-0 left-0 h-screen w-screen z-50">
-    <div class="flex items-center gap-2 justify-center flex-col h-full w-full bg-white">
+    <div class="flex items-center justify-center flex-col h-full w-full bg-white">
+        <img src="{ assets }/images/icons/alert.svg" alt="" class="h-16">
         <h2 class="text-2xl">This site is a work in progress</h2>
         <p class="w-1/2 text-center">This site is in an incomplete state, however, if you still wish to proceed, please click the button below</p>
         <form method="dialog">
-            <button class="p-2 mt-2 bg-black text-white rounded-lg">Proceed</button>
+            <button class="p-2 mt-4 bg-black text-white rounded-lg">Proceed</button>
         </form>
     </div>
 </dialog>
