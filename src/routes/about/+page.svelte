d36ebae860ecfe881459d64c97ca8e6dc1daebb6
AustinCGause <austincgause@gmail.com>
2024-03-15 00:42:06 -0400

    added emoji to about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,7 +6,7 @@
 <section class="mb-16">
     <ProseArticle>
         <h3 class="dark:text-white">The Amazing World of <strike>Gumball</strike> Austin</h3>
-        <p>Greetings, earthlings! I'm Austin, a sophomore Computer Science major with a minor in Math at Valdosta State University. By day, I'm a freelance web developer and full-time student with an interest in CLI applications and foreign languages. By night, I still have the same responsibilities, just with a little bit of exhaustion mixed in.</p>
+        <p>Greetings, earthlings &#128406! I'm Austin, a sophomore Computer Science major with a minor in Math at Valdosta State University. By day, I'm a freelance web developer and full-time student with an interest in CLI applications and foreign languages. By night, I still have the same responsibilities, just with a little bit of exhaustion mixed in.</p>
         <p>However, my journey into this realm didn't start with web development. My initial foray into technology started when I was</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quia recusandae pariatur veniam eius dolore quaerat iure, magni cupiditate laboriosam eos velit itaque voluptatum sunt impedit eaque consequuntur ipsam! Voluptas!</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quia recusandae pariatur veniam eius dolore quaerat iure, magni cupiditate laboriosam eos velit itaque voluptatum sunt impedit eaque consequuntur ipsam! Voluptas!</p>
