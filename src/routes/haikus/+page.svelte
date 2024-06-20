ba6ec34fb56dcbd4381d32f4d707199f874983d9
Colt <austincgause@gmail.com>
2024-06-20 11:20:20 -0400

    Feat(route): added a new route, haikus
    A more or less "secret route", there is no visible button for this
    route, but it is accessible to anyone who knows about it

---
+++ src/routes/haikus/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import ProseArticle from "$lib/general/ProseArticle.svelte";
+</script>
+
+<section class="mb-16 flex">
+    <ProseArticle>
+        <h3 class="text-center dark:text-white">Haikus</h3>
+        <p class="dark:text-white font-bold">Fortnite</p>
+        <p class="">
+            Fortnite, oh forntite<br />
+            do thou not play fortnite<br />
+            for it is good
+        </p>
+        <hr>
+        <p class="">
+            Fortnite, oh forntite<br />
+            do thou not play fortnite<br />
+            for it is good
+        </p>
+    </ProseArticle>
+</section>
