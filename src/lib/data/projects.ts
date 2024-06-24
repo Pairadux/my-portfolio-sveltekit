c08a01ded34006318dbfaa0bb8c70ac9530afeb0
Colt <austincgause@gmail.com>
2024-06-24 10:30:50 -0400

    Feat(projects): added href to projects and wrapped project in anchor tag

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -2,6 +2,7 @@ export interface projectItem {
     image: string;
     title: string;
     description: string;
+    href: string;
     featured: boolean;
 }
 
@@ -10,72 +11,84 @@ export const projects: projectItem[] = [
         image: "dirty-beach.jpg",
         title: "Dirty Beach Cleans",
         description: "A beachy themed cleaning website designed to bring attention to a local, small business.",
+        href: "dirty-beach-cleans",
         featured: true
     },
     {
         image: "best-friends-dog-rescue.jpg",
         title: "Best Friends Dog Rescue",
         description: "A website filled with furry friends to replace the client's outdated site.",
+        href: "best-friends-dog-rescue",
         featured: true
     },
     {
         image: "table-tennis-site.jpg",
         title: "Table Tennis Site",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        href: "table-tennis-site",
         featured: true
     },
     {
         image: "hospital-queue-app.jpg",
         title: "Hospital Queue App*",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        href: "hospital-queue-app",
         featured: true
     },
     {
         image: "command-line-apps.jpg",
         title: "Rust CLI App*",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        href: "rust-cli-app",
         featured: true
     },
     {
         image: "cross-platform-app.jpg",
-        title: "Cross-platform Flashcard App*",
+        title: "Cross-platform App*",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        href: "cross-platform-flashcard-app",
         featured: true
     },
     {
         image: "cross-platform-app.jpg",
         title: "EXAMPLE",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        href: "test",
         featured: false
     },
     {
         image: "command-line-apps.jpg",
         title: "EXAMPLE",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        href: "test",
         featured: false
     },
     {
         image: "hospital-queue-app.jpg",
         title: "EXAMPLE",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        href: "test",
         featured: false
     },
     {
         image: "cross-platform-app.jpg",
         title: "EXAMPLE",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        href:"test",
         featured: false
     },
     {
         image: "command-line-apps.jpg",
         title: "EXAMPLE",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        href: "test",
         featured: false
     },
     {
         image: "hospital-queue-app.jpg",
         title: "EXAMPLE",
         description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+        href: "test",
         featured: false
     },
 ];
--- src/lib/general/ProjectItem.svelte
+++ src/lib/general/ProjectItem.svelte
@@ -3,35 +3,38 @@
     export let title: string;
     export let description: string;
     export let image: string;
+    export let href: string;
 </script>
 
-<div class="relative h-72 w-72 group transition-transform hover:transform hover:-translate-x-1 hover:-translate-y-1">
+<a href="/projects/{href}" class="no-underline">
+    <div class="relative h-72 w-72 group transition-transform hover:transform hover:-translate-x-1 hover:-translate-y-1">
 
-    <div class="absolute group-hover:left-4 group-hover:top-4 transition-all left-2 top-2 h-[95%] w-[95%] outline-dashed outline-offset-2 outline-blue-500"></div>
+        <div class="absolute group-hover:left-4 group-hover:top-4 transition-all left-2 top-2 h-[95%] w-[95%] outline-dashed outline-offset-2 outline-blue-500"></div>
 
-    <!-- PORTFOLIO ITEMS -->
-    <div class="relative flex h-full w-full justify-center border-[1.5px] border-black dark:border-white bg-white shadow-md">
+        <!-- PORTFOLIO ITEMS -->
+        <div class="relative flex h-full w-full justify-center border-[1.5px] border-black dark:border-white bg-white shadow-md">
 
-        <!-- PORTFOLIO HOVER ITEMS -->
-        <div class="absolute left-1/2 top-1/2 flex h-full w-full bg-blue-500 -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-2 px-4">
-            <h2 class="text-center text-2xl heading-font text-white transition md:text-3xl">
+            <!-- PORTFOLIO HOVER ITEMS -->
+            <div class="absolute left-1/2 top-1/2 flex h-full w-full bg-blue-500 -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-2 px-4">
+                <h2 class="text-center text-2xl heading-font text-white transition md:text-3xl">
+                    {title}
+                </h2>
+                <p class="text-center text-lg text-white md:text-xl">
+                    {description}
+                </p>
+            </div>
+
+            <!-- PORTFOLIO IMAGE -->
+            <img
+                class="object-cover brightness-50 transition-opacity group-hover:opacity-0"
+                src="{assets}/images/portfolio/{image}"
+                alt="" />
+
+            <!-- PORTFOLIO TITLE -->
+            <h2 class="absolute left-1/2 top-1/2 w-full heading-font group-hover:invisible group-hover:opacity-0 transition-opacity -translate-x-1/2 -translate-y-1/2 px-1 text-center text-2xl text-white md:text-3xl">
                 {title}
             </h2>
-            <p class="text-center text-lg text-white md:text-xl">
-                {description}
-            </p>
+
         </div>
-
-        <!-- PORTFOLIO IMAGE -->
-        <img
-            class="object-cover brightness-50 transition-opacity group-hover:opacity-0"
-            src="{assets}/images/portfolio/{image}"
-            alt="" />
-
-        <!-- PORTFOLIO TITLE -->
-        <h2 class="absolute left-1/2 top-1/2 w-full heading-font group-hover:invisible group-hover:opacity-0 transition-opacity -translate-x-1/2 -translate-y-1/2 px-1 text-center text-2xl text-white md:text-3xl">
-            {title}
-        </h2>
-
     </div>
-</div>
+</a>
