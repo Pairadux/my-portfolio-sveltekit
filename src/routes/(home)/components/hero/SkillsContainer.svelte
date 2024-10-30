35596e3d1bd126dbebbc75e30974c2b0fcec1445
Pairadux <austincgause@gmail.com>
2024-10-30 11:48:37 -0400

    Style(home:skills): adjusted the point at which skills flex

--- src/routes/(home)/components/hero/SkillsContainer.svelte
+++ src/routes/(home)/components/hero/SkillsContainer.svelte
@@ -14,7 +14,7 @@
     <p class="text-center font-light leading-none tracking-tight text-neutral-500 md:hidden">Tap for details</p>
 
     <!-- SKILLS -->
-    <div class="mt-8 grid grid-cols-5 gap-4 md:flex">
+    <div class="mt-8 grid grid-cols-5 gap-4 lg:flex">
         {#each skills as skill}
             <Skill {skill} />
         {/each}
