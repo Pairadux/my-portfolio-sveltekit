169c5bae8cffe7bd7aa7a8eaac51764a169e3fbd
Pairadux <austincgause@gmail.com>
2024-10-30 18:34:52 -0400

    Feat(about): fill out about page, add additional images

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,26 +1,3 @@
-<!-- <script lang="ts"> -->
-<!--     import { assets } from "$app/paths"; -->
-<!--     import ProseArticle from "$lib/general/ProseArticle.svelte"; -->
-<!-- </script> -->
-<!---->
-<!-- <section class="mb-16"> -->
-<!--     <ProseArticle> -->
-<!--         <h3 class="dark:text-white">The Amazing World of <strike>Gumball</strike> Austin</h3> -->
-<!--         <p>Greetings, earthlings &#128406! I'm Austin, a sophomore Computer Science major with a minor in Math at Valdosta State University. By day, I'm a freelance web developer and full-time student with an interest in CLI applications and foreign languages. By night, I still have the same responsibilities, just with a little bit of exhaustion mixed in.</p> -->
-<!--         <p>However, my journey into this realm didn't start with web development. My initial foray into technology started when I was</p> -->
-<!--         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quia recusandae pariatur veniam eius dolore quaerat iure, magni cupiditate laboriosam eos velit itaque voluptatum sunt impedit eaque consequuntur ipsam! Voluptas!</p> -->
-<!--         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quia recusandae pariatur veniam eius dolore quaerat iure, magni cupiditate laboriosam eos velit itaque voluptatum sunt impedit eaque consequuntur ipsam! Voluptas!</p> -->
-<!--         <figure> -->
-<!--             <img -->
-<!--                 src="{assets}/images/portfolio/example1.jpg" -->
-<!--                 alt="" /> -->
-<!--             <figcaption>Example1</figcaption> -->
-<!--         </figure> -->
-<!--         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quia recusandae pariatur veniam eius dolore quaerat iure, magni cupiditate laboriosam eos velit itaque voluptatum sunt impedit eaque consequuntur ipsam! Voluptas!</p> -->
-<!--         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quia recusandae pariatur veniam eius dolore quaerat iure, magni cupiditate laboriosam eos velit itaque voluptatum sunt impedit eaque consequuntur ipsam! Voluptas!</p> -->
-<!--     </ProseArticle> -->
-<!-- </section> -->
-
 <script lang="ts">
     import { assets } from "$app/paths";
     import ProseArticle from "$lib/general/ProseArticle.svelte";
@@ -28,36 +5,119 @@
 
 <section class="mb-16">
     <ProseArticle>
-        <h3 class="dark:text-white">The Amazing World of <strike>Gumball</strike> Austin</h3>
-        <p>Greetings, earthlings &#128406! I'm Austin, a Computer Science major with a minor in Math at Valdosta State University. By day, I'm a freelance web developer and full-time student passionate about crafting elegant web solutions and diving into the world of command-line applications. By night, well, let's just say coffee is my most loyal companion as I juggle code, classes, and the occasional power nap.</p>
+        <h3 class="dark:text-white">
+            The Amazing World of <strike>Gumball</strike> Austin
+        </h3>
+        <p>
+            Greetings, earthlings &#128406! I'm Austin, a Computer Science major
+            with a minor in Math at Valdosta State University. I'm a network
+            student assistant, freelance web developer, and a full-time student
+            with a passion for programming, web development, Linux, and
+            command-line applications. Whether it's building responsive
+            websites, crafting efficient CLI tools, or exploring the intricacies
+            of the Linux ecosystem, I'm always eager to dive into new challenges
+            and expand my skill set.
+        </p>
 
-        <p>But my journey into technology didn't start with a keyboard and a monitor. It began on the solid ground—literally—as a flooring installer. Working with materials like Luxury Vinyl Plank and carpet taught me the value of precision, hard work, and building from the ground up. Transitioning from installing floors to writing code might seem like a leap, but both require meticulous attention to detail and a passion for creating something lasting.</p>
+        <p>
+            However, my journey into technology began long before I ever wrote a
+            line of code. Even as a child, I was captivated by the mechanics of
+            everyday devices and how they operated. Around the age of 10, I
+            started taking apart electronics, driven by a curiosity to
+            understand what made them "tick." This fascination with technology
+            and problem-solving laid the foundation for my career today. While I
+            spent some time as a flooring installer, learning the value of
+            precision and hard work, my passion for tech has always been at the
+            core of my personal and professional growth. Now, I channel that
+            same curiosity and attention to detail into programming, web
+            development, and exploring the command line.
+        </p>
 
         <!-- Image of you working on flooring or a symbolic image of transition -->
         <figure>
-            <img src="{assets}/images/about/people-renovating-house-concept.jpg" alt="From Flooring to Coding" />
-            <figcaption>From Flooring to Coding: Building Foundations</figcaption>
+            <img
+                src="{assets}/images/about/people-renovating-house-concept.jpg"
+                alt="From Flooring to Coding" />
+            <figcaption>
+                From Flooring to Coding: Building Foundations
+            </figcaption>
         </figure>
 
-        <p>My fascination with technology was ignited during high school when I wrote my first lines of code in Python. The thrill of solving problems and bringing ideas to life propelled me into the world of programming languages like Java and Rust. At university, I've delved into web development, crafting responsive websites using Svelte and Tailwind CSS, and exploring networking fundamentals as a Network Student Assistant.</p>
+        <p>
+            My interest in programming began during high school when I first
+            attempted to write code in Python. Despite my enthusiasm, I
+            struggled to grasp many concepts due to the lack of formal teaching.
+            I spent countless hours on YouTube tutorials, managing to build
+            small apps and simple scripts but never quite feeling confident. It
+            wasn’t until the summer before my freshman year that I decided to
+            dive into web development, teaching myself the basics to gain a
+            "head start." However, it was in my CS 1010 class during freshman
+            year that everything truly clicked. With structured guidance and
+            formal instruction, programming finally started to make sense,
+            propelling me into exploring languages like Java and Rust, as well
+            as web development frameworks like Svelte and Tailwind CSS. Today,
+            as a freelance web developer and Network Student Assistant, I
+            continue to expand my skills, delving into both development and
+            networking fundamentals.
+        </p>
 
         <!-- Image of a code snippet or you working on a project -->
         <figure>
-            <img src="{assets}/images/about/html-css-collage-concept-with-person.jpg" alt="Deep in Code" />
+            <img
+                src="{assets}/images/about/html-css-collage-concept-with-person.jpg"
+                alt="Deep in Code" />
             <figcaption>Lost in Lines of Code</figcaption>
         </figure>
 
-        <p>Beyond the screen, I'm an avid enthusiast of foreign languages and cultures. Much like programming languages, spoken languages offer new perspectives and ways of connecting with the world. I'm also an active member of the Association of Computing Machinery (ACM), where I previously served as Treasurer. Volunteering during housing move-in days has become a cherished tradition, allowing me to give back to the campus community.</p>
+        <p>
+            Beyond the screen, I'm drawn to exploring a variety of interests. I
+            have a genuine love for foreign languages and cultures, along with a
+            deep appreciation for TV, music, gaming, and reading. Building
+            computers has also become an interesting, hands-on hobby, letting me
+            combine my technical skills with my craftsmanship. Gaming,
+            especially, is a big passion, and it often fuels my curiosity and
+            excitement for exploring new tech.
+        </p>
+
+        <figure>
+            <img
+                src="{assets}/images/about/gaming.jpg"
+                alt="Immersed in Gaming" />
+            <figcaption>Immersed in Gaming</figcaption>
+        </figure>
+
+        <p>
+            I’m also active on campus as a member of the Association of
+            Computing Machinery (ACM), where I previously served as Treasurer. I
+            regularly attend on-campus events and occasionally volunteer on
+            campus. Volunteering has given me the chance to connect with others
+            and contribute to a supportive community.
+        </p>
 
         <!-- Image of you participating in ACM or volunteering -->
         <figure>
-            <img src="{assets}/images/about/different-people-doing-volunteer-work-with-food.jpg" alt="ACM Volunteering" />
+            <img
+                src="{assets}/images/about/different-people-doing-volunteer-work-with-food.jpg"
+                alt="ACM Volunteering" />
             <figcaption>Community First: Volunteering with ACM</figcaption>
         </figure>
 
-        <p>When I'm not coding or studying, you might find me experimenting with new CLI applications, sipping on a freshly brewed cup of coffee, or planning my next adventure into a foreign language. I'm always eager to tackle new challenges, collaborate on exciting projects, and continue growing both personally and professionally.</p>
+        <p>
+            When I'm not coding or studying, you might find me experimenting
+            with new CLI applications, working on personal projects, or
+            researching emerging technologies. I'm always eager to tackle new
+            challenges, collaborate on exciting projects, and continue growing
+            both personally and professionally.
+        </p>
 
-        <p>Feel free to reach out if you'd like to collaborate, chat about tech, or share your favorite coffee recipe. Let's build something amazing together!</p>
+        <p>
+            Feel free to reach out if you'd like to collaborate, chat about
+            tech, or <a
+                href="https://www.youtube.com/watch?v=jabH3wRwiWQ"
+                class="italic text-blue-500"
+                >discuss the political and economic state of the world right
+                now.</a>
+            Let's build something amazing together!
+        </p>
     </ProseArticle>
 </section>
-
