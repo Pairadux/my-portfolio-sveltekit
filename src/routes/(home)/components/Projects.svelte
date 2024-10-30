21c3b7be1635537ceda8c03ff72257ccc4311ed8
Pairadux <austincgause@gmail.com>
2024-10-30 12:11:02 -0400

    Style(home:projects): updated projects to be grid-cols-2 on md and wrapped header in div

--- src/routes/(home)/components/Projects.svelte
+++ src/routes/(home)/components/Projects.svelte
@@ -8,29 +8,33 @@
 <section class="flex flex-col items-center lg:w-full">
 
     <!-- TITLE -->
-    <SectionHeader >
-        <svelte:fragment slot="header">Recent Projects</svelte:fragment>
-        <svelte:fragment slot="subheader">
-            A few of my most recent projects. Want to see more? Head over to my
-            <span
-            ><a
-                    class="text-blue-500"
-                    href="/projects">projects page</a
-                ></span
-            >.
-        </svelte:fragment>
-    </SectionHeader>
+    <div class="w-4/5">
+        <SectionHeader >
+            <svelte:fragment slot="header">Recent Projects</svelte:fragment>
+            <svelte:fragment slot="subheader">
+                A few of my most recent projects. Want to see more? Head over to my
+                <span
+                ><a
+                        class="text-blue-500"
+                        href="/projects">projects page</a
+                    ></span
+                >.
+            </svelte:fragment>
+        </SectionHeader>
+    </div>
  
     <p class="text-sm dark:text-white">* = unfinished projects</p>
 
     <!-- PORTFOLIO ITEMS CONTAINER -->
-    <div class="flex mt-8 flex-col gap-8 lg:grid lg:grid-cols-3">
+    <div class="flex mt-8 flex-col gap-8 md:grid md:grid-cols-2 lg:grid-cols-3">
         {#each projects as project}
             {#if project.featured}
                 <ProjectItem
                     title={project.title}
                     description={project.description}
-                    image={project.image} />
+                    image={project.image} 
+                    href={project.href}
+                />
             {/if}
         {/each}
     </div>
