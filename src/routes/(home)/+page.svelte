99fec83cea681f41fc0971b1ce90265ea44de76f
Pairadux <austincgause@gmail.com>
2024-10-30 20:57:58 -0400

    Feat: add testimonial section to home page

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
     // import Hero from "./components/hero/Hero.svelte";
-    import Hero from "./components/Hero.svelte"
+    import Hero from "./components/Hero.svelte";
     // import Greeting from "./components/Greeting.svelte";
     import SkillsContainer from "./components/SkillsContainer.svelte";
     import Projects from "./components/Projects.svelte";
-    // import Testimonials from "./components/Testimonials.svelte";
+    import Testimonials from "./components/Testimonials.svelte";
     import Cta from "./components/CTA.svelte";
 </script>
 
@@ -21,12 +21,11 @@
         <hr class="my-16 h-px w-3/4 border-[1.5px] border-dashed border-black dark:border-white" />
 
         <Projects />
-
-        <!-- <hr class="my-8 h-px border-[1.5px] w-3/4 border-black border-dashed dark:border-white" /> -->
-        <!---->
-        <!-- <Testimonials /> -->
     </div>
 
-    <!-- ADD CALL TO ACTION -->
     <Cta />
+
+    <div class="container m-auto flex flex-col items-center">
+        <Testimonials />
+    </div>
 </div>
--- src/routes/(home)/components/Testimonials.svelte
+++ src/routes/(home)/components/Testimonials.svelte
@@ -1,9 +1,32 @@
 <script lang="ts">
-    import SectionHeader from "$lib/general/SectionHeader.svelte";
+    import { assets } from "$app/paths";
 </script>
 
-<section class="my-16 flex w-3/4 flex-col items-center gap-12 lg:w-full">
-    <div>
-        <SectionHeader title="Testimonials">What some of the people I've worked with in the past to say about me.</SectionHeader>
-    </div>
+<section class="flex w-full flex-col items-center">
+    <figure class="w-4/5 px-2">
+        <blockquote class="text-center text-xl/8 font-semibold sm:text-2xl/9 dark:text-white">
+            <p>"Austin is pretty cool, makes some cool websites and stuff like that"</p>
+        </blockquote>
+        <figcaption class="mt-10">
+            <img
+                class="mx-auto h-10 w-10 rounded-full"
+                src="{assets}/images/headshot/senior_photo_headshot_blue_bg.jpg"
+                alt="" />
+            <div class="mt-4 flex items-center justify-center space-x-3 text-base">
+                <div class="font-semibold dark:text-white">Austin Gause</div>
+                <svg
+                    viewBox="0 0 2 2"
+                    width="3"
+                    height="3"
+                    aria-hidden="true"
+                    class="fill-black dark:fill-white">
+                    <circle
+                        cx="1"
+                        cy="1"
+                        r="1" />
+                </svg>
+                <div class="dark:text-white">MAKER of WEBSITE</div>
+            </div>
+        </figcaption>
+    </figure>
 </section>
