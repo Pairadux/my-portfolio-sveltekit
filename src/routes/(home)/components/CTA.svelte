f1a32fcf003ab341e2ed3669134e457980688259
Pairadux <austincgause@gmail.com>
2024-10-30 20:55:06 -0400

    Fix(CTA): adjust CTA to include meaningful information and adjust margins

--- src/routes/(home)/components/CTA.svelte
+++ src/routes/(home)/components/CTA.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 </script>
 
-<section class="mt-16 flex w-full flex-col items-center gap-2 bg-blue-500 py-8">
+<section class="my-16 flex w-full flex-col items-center gap-2 bg-blue-500 py-8">
     <p class="text-center text-3xl font-bold text-white">Need to chat?</p>
-    <p class="text-center text-white font-bold"><a href="https://linktr.ee/AustinGause">TEMP</a></p>
+    <p class="text-center text-white font-bold">Check out my linktree &rightarrow; <a class="italic" href="https://linktr.ee/AustinGause">here</a> &leftarrow;</p>
 </section>
