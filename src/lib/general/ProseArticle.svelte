42f959807db87584fd2f64074a02ae3d3def018e
Pairadux <austincgause@gmail.com>
2024-11-06 22:26:31 -0500

    Fix(ProseArticle): update method of text color inversion

--- src/lib/general/ProseArticle.svelte
+++ src/lib/general/ProseArticle.svelte
@@ -1,3 +1,3 @@
-<article class="container prose prose-base prose-slate dark:text-white m-auto px-4 sm:prose-lg md:prose-xl">
+<article class="container prose prose-base prose-slate dark:prose-invert m-auto px-4 sm:prose-lg md:prose-xl">
     <slot />
 </article>
